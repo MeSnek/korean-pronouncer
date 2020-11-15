2d9b38f03143d3ceba5f73ef4129e673a9c23dbf
MeSnek <sneakyfox61@gmail.com>
2020-11-15 00:12:48 -0500

    first commit

---
+++ target/debug/deps/kr_pronouncer-c93b5bd467691a8a.d
@@ -0,0 +1,5 @@
+d:\programming\rust\kr-pronouncer\target\debug\deps\kr_pronouncer-c93b5bd467691a8a.rmeta: src\main.rs
+
+d:\programming\rust\kr-pronouncer\target\debug\deps\kr_pronouncer-c93b5bd467691a8a.d: src\main.rs
+
+src\main.rs:
