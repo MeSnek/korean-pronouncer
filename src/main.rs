787f5c009c9f693641f9d5c2ca9b4d7c9c58f6d2
MeSnek <sneakyfox61@gmail.com>
2020-11-19 06:06:58 -0500

    added stuff, might have to go back after i change it

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
 use hangul::HangulExt;
+use unic_ucd_hangul::compose_syllable;
+use std::collections::HashMap;
 
-fn pronounced(hangul: &String, ) { 
- 
-}
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let hangul: &String = &args[1];
-    let hangul: String = hangul.to_string();
-    
-    let vector: Vec<char> = hangul.chars().flat_map(|c| c.jamos().unwrap()).collect();
-    let mut i: i32 = 1;
+    let hangul = String::from(&args[1]);
+    let hangul = &hangul[..];
 
-    println!("You entered: {}", hangul);
-    print!("{} is comprised of: ", hangul);
-    
-    for elem in vector.iter() {
-        print!("{}, ", elem);
-    }
+    let mut hashmap: HashMap<&str, Vec<&str> > = HashMap::new();
+
+    fill_hashmap(hangul, &mut hashmap);
+
+    //let vector: Vec<char> = hangul.chars().flat_map(|c| c.jamos().unwrap()).collect();
+
+    // println!("You entered: {}", hangul);
+    // print!("{} is comprised of: ", hangul);
 }
+
+fn fill_hashmap(hangul: &str, hashmap: &mut HashMap<&str, Vec<&str> > ) {
+    let vec = split(hangul);
+    let vec = decomp(vec);
+
+    //create hashmap
+
+}
+
+fn split(hangul: &str) -> Vec<&str> { 
+    let vec: Vec<&str> = hangul.split(' ').collect();
+    return vec;
+}
+
+fn decomp(original: Vec<&str>) -> Vec<char> { 
+    let mut vec: Vec<char> = Vec::new();
+    
+    for elem in original.into_iter() {
+        let s: String = String::from(elem);
+        
+        vec.push(s.chars().flat_map(|c| c.jamos().unwrap()).collect());
+        //vec.push(elem.to_string().chars().flat_map(|c| c.jamos().unwrap()).collect());
+        //vec.push(elem.chars().flat_map(|c| c.jamos().unwrap()).collect());
+    }
+    return vec;
+}
+
 //TODO: start implementing the rules. not sure where to start, 받침 or the otherweird ones, look into er
+fn pronounce(hangul: &String) {  // maybe use &str slice type for generic? 
+    
+}
