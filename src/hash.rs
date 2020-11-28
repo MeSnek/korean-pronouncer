64fed1177807545b6b6bf998a47d06b1e6b0649b
MeSnek <sneakyfox61@gmail.com>
2020-11-27 22:20:42 -0500

    last commit before doing pronunciation rules

--- src/hash.rs
+++ src/hash.rs
@@ -38,6 +38,34 @@ fn decomp(original: Vec<&str>) -> Vec<char> {
 
 //TODO: start implementing the rules. not sure where to start, 받침 or the otherweird ones, look into er
 //REMEBER: make sure u dont transfer ownership of decomps values, because u need them to imake the hangulinfo struct in fill_hashmap
+//will not be doing 받침 theres ez af just learn em lol
+//also not able to really do anything relateed to if something is grammatical or not
+/*
+
+    ㄱㄲㅋ ㅈㅊ     ㄷㅌ ㅂㅍ   ㅅㅆ    ㅁㄴㅇㄹㅎ      possible as last letter
+    3 1 1  2 2     2 2  3 1    2 2    1 1 1 0 4      possible rules
+
+    nasalization
+        ㄱㄲㅋ ->               ㅇ if before ㄴ/ㅁ 격노 -> 경노 / 국물 -> 궁물
+        ㄷㅌㅅㅆㅈㅊㅎ ->        ㄴ if before ㄴ/ㅁ 
+        ㅂㅃㅍ ->               ㅁ if before ㄴ/ㅁ
+    aspiration
+        ㅎ ->                   ㅌ if before ㄷ
+        ㅎ ->                   ㅊ if before ㅈ
+        ㅎ ->                   ㅆ if before ㅅ
+        ㅎ ->                   ㅋ if before ㄱ
+
+        ㄱ ->                   ㅋ if before ㅎ 
+        ㅂ ->                   ㅍ if before ㅎ
+        ㄷㅌㅅㅆㅈㅊㅎ ->        ㅌ if before ㅎ
+    assim / alterations with ㄹ
+        ㅁㄹ -> ㅁㄴ
+        ㅇㄹ -> ㅇㄴ
+        ㄱㄹ -> ㅇㄴ
+        ㅂㄹ -> ㅁㄴ
+        ㄴㄹ -> ㄹㄹ EXCEPTION {의견란[의ː견난]  임진란[임ː진난]  생산량[생산냥]  결단력[결딴녁] 공권력[공꿘녁]  동원령[동ː원녕]  상견례[상견녜]  횡단로[횡단노] 이원론[이ː원논]  입원료[이붠뇨]  구근류[구근뉴]}
+
+*/
 fn pronounce(decomp: &Vec<char>) -> String {
     return String::from("fuck");
 }
--- src/output.rs
+++ src/output.rs
@@ -4,21 +4,25 @@ use super::hash::HangulInfo;
 //-h    help 
 //-i    outputs like how the for loop is now, serperate each word
 //-s    outputs a whole sentence without serperating each word\
-pub fn output(p: String, hashmap: &mut HashMap<String, HangulInfo>, order: Vec<String>) {
-    match p.as_str() {
+pub fn output(param: String, hashmap: &mut HashMap<String, HangulInfo>, order: Vec<String>) {
+    match param.as_str() {
         "-h" | "-H" => help(),
         "-i" | "-I" => individual(hashmap, order),
-        "-s" | "-S" => sentence(hashmap, order),
+        "-s" | "-S" => sentence(hashmap, order), 
         _=>            individual(hashmap, order),
     }
 }
 
 pub fn help() {
     print!(
-"krpr is a program that tells you how to pronounce korean words/sentences.
-I wrote this for personal use so dont expect it to be perfect.
-The default mode for krpr is -i, which outputs each word seperately. 
+"krpr is a program that tells you how to pronounce korean words/sentences,
+following the advanced pronunciation rules found here:
+https://en.wikibooks.org/wiki/Korean/Advanced_Pronunciation_Rules.
+Basically why I'm saying this is for you to know that this is for people that
+already know how to pronounce korean, and want to sound more natural.
+I wrote this for personal use, so dont expect it to be perfect.
 
+The default mode for krpr is -i, which outputs each word seperately. 
 You can specify what mode you want to use by adding a parameter in this format, 
     krpr [-param] (input) 
 Input can be a single word or a list of words seperated by spaces.
